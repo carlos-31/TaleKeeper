a357473d0e681a289e2bdffe629fb036a5ce2dbd
carlos-31 <dev.carlos.31@gmail.com>
2025-04-13 17:58:19 +0200

    Glide implementation

--- TaleKeeper/app/build.gradle.kts
+++ TaleKeeper/app/build.gradle.kts
@@ -53,5 +53,6 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
+    implementation(libs.glide)
 
 }
