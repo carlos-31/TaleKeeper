95cdfb3daca578630817a1f836bbcc96b29f3985
carlos-31 <dev.carlos.31@gmail.com>
2025-05-01 17:24:44 +0200

    Modified xml and prepared fragment for databindig

--- TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/viewmodels/DiscoverViewModel.java
+++ TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/viewmodels/DiscoverViewModel.java
@@ -6,14 +6,4 @@ import androidx.lifecycle.ViewModel;
 
 public class DiscoverViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
-
-    public DiscoverViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
--- TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/views/DiscoverFragment.java
+++ TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/views/DiscoverFragment.java
@@ -1,30 +1,43 @@
 package com.carlosreads.talekeeper.views;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.carlosreads.talekeeper.databinding.FragmentDiscoverBinding;
+import com.carlosreads.talekeeper.databinding.FragmentHomeBinding;
 import com.carlosreads.talekeeper.viewmodels.DiscoverViewModel;
 
 public class DiscoverFragment extends Fragment {
-
+    private DiscoverViewModel viewModel;
     private FragmentDiscoverBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DiscoverViewModel discoverViewModel =
-                new ViewModelProvider(this).get(DiscoverViewModel.class);
+        viewModel = new ViewModelProvider(this).get(DiscoverViewModel.class);
 
         binding = FragmentDiscoverBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        binding.setViewModel(viewModel);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+
+        binding.searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(requireContext(), binding.search.getText() , Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return root;
     }
