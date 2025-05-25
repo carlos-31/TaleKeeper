c5415e77dca78164a56a27c6d9b00d27d73eeddb
carlos-31 <dev.carlos.31@gmail.com>
2025-05-25 16:48:04 +0200

    Implementation of the dark mode switch in settings

--- TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/viewmodels/SettingsViewModel.java
+++ TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/viewmodels/SettingsViewModel.java
@@ -1,7 +1,23 @@
 package com.carlosreads.talekeeper.viewmodels;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.carlosreads.talekeeper.repositories.UserRepository;
+import com.carlosreads.talekeeper.views.SettingsFragment;
+
 public class SettingsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private UserRepository userRepository;
+
+    public SettingsViewModel(){
+        userRepository = new UserRepository();
+
+    }
+
+    private void checkDarkMode() {
+
+    }
 }
--- TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/views/SettingsFragment.java
+++ TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/views/SettingsFragment.java
@@ -1,13 +1,17 @@
 package com.carlosreads.talekeeper.views;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.PaintFlagsDrawFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -29,6 +33,12 @@ public class SettingsFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         binding.setLifecycleOwner(getViewLifecycleOwner());
 
+        setUpUI();
+
+        return binding.getRoot();
+    }
+
+    private void setUpUI() {
         Bundle args = getArguments();
         if (args != null)
             loggedIn = args.getBoolean("userStatus", false);
@@ -40,7 +50,26 @@ public class SettingsFragment extends Fragment {
         else
             binding.accountSettings.setVisibility(View.GONE);
 
-        return binding.getRoot();
+        SharedPreferences prefs = requireActivity().getSharedPreferences("AppConfig", Context.MODE_PRIVATE);
+        boolean isDarkMode = prefs.getBoolean("darkMode", false);
+        binding.themeSwitch.setChecked(isDarkMode);
+
+        setUpListeners(prefs);
     }
+
+    private void setUpListeners(SharedPreferences prefs) {
+        binding.themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+                prefs.edit().putBoolean("darkMode", isChecked).apply();
+            }
+        });
+    }
+
 
 }
