e674a74c633192d046eade0f31ba32ad003b0a88
carlos-31 <dev.carlos.31@gmail.com>
2025-05-30 23:47:37 +0200

    Fixed scrolling in LoginActivity. Made changes to the layout

--- TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/views/LoginActivity.java
+++ TaleKeeper/app/src/main/java/com/carlosreads/talekeeper/views/LoginActivity.java
@@ -33,7 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         binding.setViewModel(viewModel);
         binding.setLifecycleOwner(this);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
